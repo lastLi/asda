248263ab51ea2e5271aa2256b169c3bf0f30743c
lastLi <45660866+lastLi@users.noreply.github.com>
2019-03-26 12:04:24 +0800

    helloworld

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
-//@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(DemoApplication.class, args);
-        System.out.println("heeeee");
+        SpringApplication.run(DemoApplication.class, args);
+
     }
 
 }
